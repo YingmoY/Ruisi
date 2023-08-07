c3c5b5d2df7a97b1460caf480b6f49c1818d6053
YingMo <94264391+YingmoY@users.noreply.github.com>
2023-08-08 07:20:03 +0800

    Update index.php
    Automatically determine whether the field is empty.

--- index.php
+++ index.php
@@ -27,13 +27,13 @@
         <input type="text" id="vocabularyCount" name="vocabularyCount" required><br><br>
         
         <label for="dictationAccuracy">默写正确率：</label>
-        <input type="text" id="dictationAccuracy" name="dictationAccuracy" required><br><br>
+        <input type="text" id="dictationAccuracy" name="dictationAccuracy"><br><br>
         <script>  
             document.getElementById('dictationAccuracy').value='%';  
         </script>
         
         <label for="softwareAccuracy">软件正确率：</label>
-        <input type="text" id="softwareAccuracy" name="softwareAccuracy" required><br><br>
+        <input type="text" id="softwareAccuracy" name="softwareAccuracy"><br><br>
         <script>  
             document.getElementById('softwareAccuracy').value='%';  
         </script>
@@ -42,10 +42,10 @@
         <textarea id="comment" name="comment" rows="6" cols="50" required></textarea><br><br>
         
         <label for="dictationScores">默写情况：</label><br>
-        <textarea id="dictationScores" name="dictationScores" rows="4" cols="50" required></textarea><br><br>
+        <textarea id="dictationScores" name="dictationScores" rows="4" cols="50"></textarea><br><br>
         
         <label for="wordsToReview">需要注意复习的单词：</label><br>
-        <textarea id="wordsToReview" name="wordsToReview" rows="2" cols="50" required>no信封中的单词</textarea><br><br>
+        <textarea id="wordsToReview" name="wordsToReview" rows="2" cols="50">no信封中的单词</textarea><br><br>
         
         <label for="homework">今日作业：</label><br>
         <textarea id="homework" name="homework" rows="2" cols="50" required>
@@ -74,30 +74,52 @@
             // 输出学习报告
             echo "<h2>{$date} {$studentName} Report</h2>";
             echo "《单词速记素质能力课程》</br>";
+            
+            // 学习速记学习情况及反馈
             echo "<h3>① 单词速记学习情况及反馈：</h3></br>";
             echo "<p><strong>🎓学生: {$studentName}</strong></p>";
             echo "<p>⏰ 学习时间：{$studyTime}</p>";
             echo "<p>📍 今日学习内容：{$studyContent}</p>";
             echo "<p>📖学习/复习词汇量：{$vocabularyCount}</p>";
+            
+            // 默写正确率
+            if (!empty($dictationAccuracy)) {
             echo "<p>📈默写正确率：{$dictationAccuracy}</p>";
-            echo "<p>🏷软件正确率：{$softwareAccuracy}</p>";
-            
-            echo "<h3>②学习态度及课堂表现：</h3>";
-            echo "<p>{$comment}</p></br>";
-            
-            echo "<h3>🌟默写情况：</h3>";
-            echo "<pre>{$dictationScores}</pre></br>";
-            
-            echo "<h3>🌟需要注意复习的单词：</h3>";
-            echo "<pre>{$wordsToReview}</pre></br>";
-            
-            echo "<h3>③今日作业：</h3>";
-            echo "<ol>";
-            $tasks = explode("\n", $homework);
-            foreach ($tasks as $task) {
-                echo "<li>{$task}</li>";
             }
-            echo "</ol>";
+
+            // 软件正确率
+            if (!empty($softwareAccuracy)) {
+            echo "<p>🏷软件正确率：{$softwareAccuracy}</p>";
+            }
+
+            // 学习态度及课堂表现
+            if (!empty($comment)) {
+                echo "<h3>②学习态度及课堂表现：</h3>";
+                echo "<p>{$comment}</p></br>";
+            }
+            
+            // 默写情况
+            if (!empty($dictationScores)) {
+                echo "<h3>🌟默写情况：</h3>";
+                echo "<pre>{$dictationScores}</pre></br>";
+            }
+            
+            // 需要注意复习的单词
+            if (!empty($wordsToReview)) {
+                echo "<h3>🌟需要注意复习的单词：</h3>";
+                echo "<pre>{$wordsToReview}</pre></br>";
+            }
+            
+            // 今日作业
+            if (!empty($homework)) {
+                echo "<h3>③今日作业：</h3>";
+                echo "<ol>";
+                $tasks = explode("\n", $homework);
+                foreach ($tasks as $task) {
+                    echo "<li>{$task}</li>";
+                }
+                echo "</ol>";
+            }
         }
     ?>
 </body>
